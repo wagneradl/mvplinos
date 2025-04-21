9ca4936c4e42b09e760c87eec30152dfbe08b2fa
Wagner <wagneradl@me.com>
2025-04-21 10:34:54 -0300

    atualização

--- packages/frontend/src/app/metadata.ts
+++ packages/frontend/src/app/metadata.ts
@@ -4,4 +4,13 @@ export const metadata: Metadata = {
   title: "Lino's Panificadora - Sistema de Gestão de Pedidos",
   description: 'Sistema de gestão de pedidos da panificadora do Lino',
   viewport: 'width=device-width, initial-scale=1',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/favicon.ico',
+    apple: '/apple-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-icon.png',
+    },
+  },
 };
